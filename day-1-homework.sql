ca007ad251e5bcde09b43842e0e35347afcb45c8
Darin Son <dson64@outlook.com>
2021-04-05 13:48:52 -0700

    updates question 3

--- day-1-homework.sql
+++ day-1-homework.sql
@@ -1,8 +1,9 @@
 -- Question 1: How many actors are there with the last name ‘Wahlberg’?
 -- 2
-SELECT *
+SELECT last_name, COUNT(actor_id)
 FROM actor
-WHERE last_name = 'Wahlberg';
+WHERE last_name = 'Wahlberg'
+GROUP BY last_name;
 
 -- Question 2: How many payments were made between $3.99 and $5.99?
 -- 5607
@@ -17,30 +18,43 @@ WHERE amount BETWEEN 3.99 and 5.99
 GROUP BY amount;
 
 -- Question 3: What film does the store have the most of? (search in inventory) 
--- for both stores?
+-- For Store_id 1, the store has the most (4 copies) of film_id 1, 4, 10, 11, 19, 22, 24, 25 and more (see query).
 SELECT store_id, film_id, COUNT(film_id)
 FROM inventory
+WHERE store_id = 1
 GROUP BY store_id, film_id
-ORDER BY store_id ASC, COUNT(film_id) DESC;
+ORDER BY COUNT(film_id) DESC, film_id ASC;
+
+-- For Store_id 2, the store has the most (4 copies) of film_id 1, 3, 8, 12, 13, 15, 21, 31, 34, 39 and more (see query).
+SELECT store_id, film_id, COUNT(film_id)
+FROM inventory
+WHERE store_id = 2
+GROUP BY store_id, film_id
+ORDER BY COUNT(film_id) DESC, film_id ASC;
 
 -- Question 4: How many customers have the last name ‘William’?
 -- 0
 SELECT last_name, COUNT(last_name)
 FROM customer
-WHERE last_name = 'William'
+WHERE last_name LIKE 'William'
+GROUP BY last_name;
+
+-- We've got 1 "Williams" and 1 "Williamson" though
+SELECT last_name, COUNT(last_name)
+FROM customer
+WHERE last_name LIKE 'William%'
 GROUP BY last_name;
 
 -- Question 5:  What store employee (get the id) sold the most rentals?
 -- staff_id = 1 sold the most rentals (8040 rentals)
-SELECT staff_id, COUNT(staff_id)
+SELECT staff_id, COUNT(staff_id) as Num_Rentals_Sold
 FROM rental
 GROUP BY staff_id;
 
 -- Question 6:  How many different district names are there?
 -- 378 different district names are present
-SELECT DISTINCT(DISTRICT)
-FROM address
-GROUP BY (DISTRICT);
+SELECT COUNT(DISTINCT(DISTRICT)) as Num_Distinct_District
+FROM address;
 
 -- Question 7: What film has the most actors in it? (use film_actor table and get film_id) 
 -- film_id = 508 has the most actors (15)
@@ -59,11 +73,11 @@ WHERE store_id = 1 and last_name LIKE '%es';
 -- Three payment amounts (2.99, 4.99 and 0.99) had number of rentals above 250
 SELECT COUNT(rental_id) as Num_Rentals, amount
 FROM payment
-WHERE customer_id BETWEEN 380 and 430
+WHERE customer_id BETWEEN 380 AND 430
 GROUP BY amount
 ORDER BY COUNT(rental_id) DESC;
 
--- Question 10: Within the film table, how many rating categories are there? And what rating has the most  movies total?
+-- Question 10: Within the film table, how many rating categories are there? And what rating has the most movies total?
 -- There are 5 rating categories, and PG-13 has the most movies.
 SELECT rating, COUNT(title) as Num_Movies
 FROM film
